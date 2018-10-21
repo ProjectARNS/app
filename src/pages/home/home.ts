8c291a9424180b8c677c98ebf63a2b6a9d6633f1
ddias5 <danilod11@live.com>
2018-10-21 07:12:40 -0300

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ChecklistPage } from '../checklist/checklist';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, navControl: NavController) {
 
+  }
+
+  onClickVamosLa() {
+    this.navCtrl.push(ChecklistPage);
   }
 
 }
---
+++ src/pages/menu/menu.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the MenuPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-menu',
+  templateUrl: 'menu.html',
+})
+export class MenuPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MenuPage');
+
+    this.showAlert();
+  }
+
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'New Alert!',
+      subTitle: 'There is a storm coming!',
+      buttons: [
+        {
+          text: 'Close'
+        },
+        {
+          text: 'Acess',
+          handler: () => {
+            this.navCtrl.push(HomePage)
+          }
+        }
+      ]
+    });
+    window.setTimeout(() => {
+      alert.present();
+    }, 2000);
+  }
+}
